a5ec3ac55f72dc361195f2a80d2de232965277ad
quansenB <inaki.breinbauer@gmail.com>
2022-01-09 11:21:13 +0100

    implement basic auth additionally

--- pages/api/trigger-lambda-function.js
+++ pages/api/trigger-lambda-function.js
@@ -3,8 +3,19 @@ import axios from "axios";
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    await axios.post(req.body.n8nWebhookUrl, req.body.forwardBody);
-    res.status(200).json({ message: 'Successfull' })
+    try{
+      const result = await axios.post(req.body.n8nWebhookUrl, req.body.forwardBody, {
+        auth: {
+          username: req.body.forwardBasicAuth.username,
+          password: req.body.forwardBasicAuth.password,
+        }
+      });
+      res.status(result.status).json({ message: result.data.message })
+    }
+    catch(err){
+      console.log(err)
+      res.status(err.response.status).json({ message: err.response.statusMessage })
+    }
   }
   else{
     res.status(400).json({ message: 'use POST with body with n8nWebhookUrl and forwardBody properties' })
